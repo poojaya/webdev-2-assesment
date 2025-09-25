cbf5b748e663a4c064dadbbc97111da509526f12
poojaya <83125968+poojaya@users.noreply.github.com>
2025-09-25 12:49:07 +1000

    Update server.js

--- WEB/server.js
+++ WEB/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+// Serve everything in the WEB folder
+app.use(express.static(__dirname));
+
+// Default route -> index.html
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
