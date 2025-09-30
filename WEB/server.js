3ff163ca06f21b59431e0ed6d22398768d0e8424
poojaya <83125968+poojaya@users.noreply.github.com>
2025-10-01 02:07:24 +1000

    Update server.js

--- WEB/server.js
+++ WEB/server.js
@@ -1,13 +1,15 @@
 // WEB/server.js
-const express = require('express');
 require('dotenv').config();
-
+const express = require('express');
+const path = require('path');
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));                 // serve /public
+app.use(express.static(path.join(__dirname, 'public')));
 
-// send index.html when hitting /
-app.get('/', (_req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/', (_req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+);
 
 app.listen(PORT, () => console.log(`WEB listening on ${PORT}`));
