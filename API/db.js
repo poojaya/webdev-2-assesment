6ec17c092a8dfa81cb9b7a9de4297605032b3b36
poojaya <83125968+poojaya@users.noreply.github.com>
2025-09-30 23:32:59 +1000

    Update db.js

--- API/db.js
+++ API/db.js
@@ -1,11 +1,17 @@
-const mysql = require('mysql2/promise');
 require('dotenv').config();
+const mysql = require('mysql2/promise');
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'charityevents_db',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10
 });
-module.exports = pool;
+
+exports.query = async (sql, params=[]) => {
+  const [rows] = await pool.execute(sql, params);
+  return rows;
+};
